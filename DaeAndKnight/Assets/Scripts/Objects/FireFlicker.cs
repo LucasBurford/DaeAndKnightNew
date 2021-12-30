9822b2fcc3cf01357316cd4e3c54d277cc7e3785
Lucas-Burford <lucasburford@live.co.uk>
2021-12-30 15:30:45 +0000

    Fixed player jumping

---
+++ DaeAndKnight/Assets/Scripts/Objects/FireFlicker.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireFlicker : MonoBehaviour
+{
+    public Light mLight;
+
+    public float brightness;
+    public float flickerRate;
+
+    // Update is called once per frame
+    void Update()
+    {
+        mLight.intensity = Mathf.PingPong(Time.time, flickerRate);
+    }
+}
