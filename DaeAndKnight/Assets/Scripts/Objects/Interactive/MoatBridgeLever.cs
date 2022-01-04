add3f452dcf2b3bbb8e859082db998dc4c2f9137
Lucas-Burford <lucasburford@live.co.uk>
2022-01-04 18:09:34 +0000

    Added moat bridge

---
+++ DaeAndKnight/Assets/Scripts/Objects/Interactive/MoatBridgeLever.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MoatBridgeLever : MonoBehaviour
+{
+    public GameObject bridge;
+
+    public TMP_Text useText;
+
+    public bool used;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") && !used)
+        {
+            useText.gameObject.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                used = true;
+                FindObjectOfType<AudioManager>().Play("Lever");
+                FindObjectOfType<AudioManager>().Play("MoatBridgeClose");
+                bridge.transform.position = new Vector3(-1.35f, 1.98f, 104.79f);
+                bridge.transform.rotation = new Quaternion(0, 0, 0, 1);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            useText.gameObject.SetActive(false);
+        }
+    }
+}
