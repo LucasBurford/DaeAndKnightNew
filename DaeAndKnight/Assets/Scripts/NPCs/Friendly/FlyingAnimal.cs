a7a4cd16567a62de9263eeca0b6eb217360846cf
Lucas-Burford <lucasburford@live.co.uk>
2022-01-13 18:04:10 +0000

    Added scenery to lake, added animals

---
+++ DaeAndKnight/Assets/Scripts/NPCs/Friendly/FlyingAnimal.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingAnimal : MonoBehaviour
+{
+    public Vector3 startPos;
+
+    public float distance;
+    public float moveSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, moveSpeed);
+
+        distance += 0.5f;
+
+        if (Vector3.Distance(transform.position, startPos) > distance)
+        {
+            Respawn();
+        }
+    }
+
+    private void Respawn()
+    {
+        transform.position = startPos;
+    }
+}
