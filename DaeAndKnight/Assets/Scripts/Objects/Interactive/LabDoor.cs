6bae89b4b3ca99d20ed7c7d08335cfc53a167128
Lucas-Burford <lucasburford@live.co.uk>
2022-01-17 18:09:17 +0000

    Added Dungeon area

---
+++ DaeAndKnight/Assets/Scripts/Objects/Interactive/LabDoor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LabDoor : MonoBehaviour
+{
+    public GameObject door;
+
+    public TMP_Text useText;
+
+    public bool used;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") && !used)
+        {
+            useText.gameObject.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                used = true;
+                FindObjectOfType<AudioManager>().Play("Lever");
+                door.transform.Rotate(-90, 180, 100);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            useText.gameObject.SetActive(false);
+        }
+    }
+}
