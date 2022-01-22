a66deaa16b295535e3726c0797873ad31ff07321
Lucas-Burford <89630296+LucasBurford@users.noreply.github.com>
2022-01-22 19:57:45 +0000

    Added castle garden area and speed up powerup

---
+++ DaeAndKnight/Assets/Scripts/Collectibles/Powerups/SpeedUp.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedUp : MonoBehaviour
+{
+    public float speed;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            FindObjectOfType<PlayerMovement>().knightMoveSpeed += speed;
+            FindObjectOfType<ItemAcquired>().AcquiredItem("Speed Up, max move speed increased!");
+            Destroy(gameObject);
+        }
+    }
+}
